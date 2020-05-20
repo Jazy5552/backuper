b11d16afb0ba69082277f7b5df2e965c97c2679e
Jazy Llerena <jazy555@hotmail.com>
2020-05-20 18:55:37 -0400

    Dont delete irrelevant old tars

--- run-backup.sh
+++ run-backup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-filename="$BACKUP_OUTPUT_DIR/${BACKUP_PREFIX}$(date +%Y-%m-%d_%H-%M).tar"
+filename="$BACKUP_OUTPUT_DIR/${BACKUP_PREFIX}.$(date +%Y-%m-%d_%H-%M).tar"
 
 cd $BACKUP_INPUT_DIR
 tar pcf $filename $BACKUP_FILES
 
 # Remove old backups
-find $BACKUP_OUTPUT_DIR -type f -name "*.tar" -mtime +$STALE_BACKUP_TIME -exec rm {} \;
+find $BACKUP_OUTPUT_DIR -type f -name "${BACKUP_PREFIX}.*.tar" -mtime +$STALE_BACKUP_TIME -exec rm {} \;
 
 echo "$(du -sh $filename)" >> /backuplog.txt
 
