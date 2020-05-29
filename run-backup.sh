b6178aed5ab76c6282721c7aa0f5849fdfe4360c
Jazy Llerena <jazy555@hotmail.com>
2020-05-29 11:59:45 -0400

    Make backups only readable by root

--- run-backup.sh
+++ run-backup.sh
@@ -3,6 +3,7 @@ filename="$BACKUP_OUTPUT_DIR/${BACKUP_PREFIX}.$(date +%Y-%m-%d_%H-%M).tar"
 
 cd $BACKUP_INPUT_DIR
 tar pcf $filename $BACKUP_FILES
+chmod 640 $filename
 
 # Remove old backups
 find $BACKUP_OUTPUT_DIR -type f -name "${BACKUP_PREFIX}.*.tar" -mtime +$STALE_BACKUP_TIME -exec rm {} \;
